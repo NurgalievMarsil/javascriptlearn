c00001c2df08cdaa70c1eab32a97920ce6e496de
NurgalievMarsil <ChechenskiyArc@yandex.ru>
2020-08-12 18:26:10 +0500

    using: append prepend before after

--- js/script.js
+++ js/script.js
@@ -1,21 +1,39 @@
 'use strict';
 
-const startButton = document.getElementById('start'),
-      incomeAddButton = document.getElementsByTagName('button'[0]),
-      expensesAddButton = document.getElementsByTagName('button'[1]),
-      depositCheck = document.querySelector('#deposit-check'),
-      additionalIncomeItem = document.querySelectorAll('.additional_income-item'),
-      budgetMonthValue = document.querySelector('.budget_month-value'),
-      budgetDayValue = document.getElementByClassName('budget_day-value'),
-      expensesMonthValue = document.getElementByClassName('expenses_month-value'),
-      additionalIncomeValue = document.getElementByClassName('additional_income-value'),
-      additionalExpensesItem = document.getElementByClassName('additional_expenses-item'),
-      incomePeriodValue = document.getElementByClassName('income_period-value'),
-      targetMonthValue = document.getElementByClassName('target_month-value'),
-      targetAmount = document.querySelector('.target-amount'),
-      periodSelect = document.querySelector('.period-select'),
-      expensesTitle = document.querySelector('.expenses-title'),
-      expensesAmount = document.querySelector('.expenses-amount'),
-      incomeTitle = document.querySelector('.income-title'),
-      incomeAmount = document.querySelector('.income-amount');
+// append - вставить в конец родителя
+// prepend - вставляет в начало родителя
+// before - вставляет перед элементом который указали
+// after - вставляет после элемента который указали
 
+// replaceWIth - вместо элемента ставится который указали
+
+// .cloneNode() - копирует лишь тег и его данные
+// .cloneNode(true) - копирует элемент полностью
+
+// textContent - добавляет текст 
+// document.createElement(tag) - создает элемент(тэг)
+
+const books = document.querySelectorAll('.book'),
+      elem = document.querySelectorAll('a'),
+      adv = document.querySelector('.adv'),
+      elem2 = document.querySelectorAll('li'),
+      newElem = document.createElement('li');
+
+books[1].after(books[0]);
+books[3].before(books[4]);
+books[5].after(books[2]);
+
+document.body.style.backgroundImage = 'url(../image/you-dont-know-js.jpg)';
+
+elem[4].textContent = 'Книга 3. this и Прототипы Объектов';
+
+adv.remove();
+
+elem2[3].after(elem2[6]);
+elem2[4].before(elem2[8]);
+
+elem2[47].after(elem2[55]);
+elem2[55].after(elem2[49]);
+elem2[49].after(elem2[50]);
+newElem.textContent = "Глава 8: За пределами ES6"
+elem2[25].append(newElem);
